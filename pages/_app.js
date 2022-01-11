fc25d358cd21c61be07d78754f31a0016b102906
jason <2B3>
2022-01-11 02:50:32 -0500

    Searchbar now styled well for large screens

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import GlobalContextAPIProvider from "../src/state-management/globalContext";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+import { customThemes } from "../styles/MUI_themes";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextAPIProvider>
-      <CssBaseline>
-        <Component {...pageProps} />
-      </CssBaseline>
+      <ThemeProvider theme={customThemes.light}>
+        <CssBaseline>
+          <Component {...pageProps} />
+        </CssBaseline>
+      </ThemeProvider>
     </GlobalContextAPIProvider>
   );
 }
--- src/custom-components/SearchbarDesktop.js
+++ src/custom-components/SearchbarDesktop.js
@@ -1,28 +1,80 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
 //  prettier-ignore
-import { Typography, Button, Box, Divider, TextField, InputBase } from "@mui/material";
+import { Typography, Button, Box, Divider, TextField, InputBase, Menu, MenuItem } from "@mui/material";
 import { styled, alpha } from "@mui/material/styles";
 import SearchIcon from "@mui/icons-material/Search";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
+import HistoryIcon from "@mui/icons-material/History";
 
 export default function SearchbarDesktop() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    // set <Search> as our anchor
+    setAnchorEl(event.currentTarget.closest("div"));
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box component="section">
       <Search>
-        <LocationOnIcon />
-        <Typography variant="p" sx={styles.location}>
-          Location
-        </Typography>
-        <ArrowDropDownIcon/>
-        <Divider orientation="vertical" flexItem sx={{mx:1}}/>
+        <Button sx={styles.menuTrigger} color="secondary" onClick={handleClick}>
+          <LocationOnIcon />
+          <Typography variant="p" sx={styles.location} align="left">
+            Location
+          </Typography>
+          <ArrowDropDownIcon />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          sx={{ mt: 0.5, "& .MuiList-root": { padding: 0 } }}
+        >
+          <MenuItem sx={{ display: "flex", p: 2 }}>
+            <GpsFixedIcon color="secondary" />
+            <Typography
+              variant="p"
+              color="secondary"
+              align="left"
+              sx={{ ml: 1 }}
+            >
+              Detect Current Location
+            </Typography>
+          </MenuItem>
+          <Box>
+            <Divider sx={{ mb: 3 }} />
+            <Typography variant="p" sx={{ fontSize: "1.125rem", ml: 2 }}>
+              Recent Locations
+            </Typography>
+            <MenuItem
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                px: 2,
+                pb: 2,
+                pt: 2,
+              }}
+            >
+              <HistoryIcon sx={{ mr: 1 }} />
+              <Box component="p" sx={{ m: 0 }}>
+                Richmond Hill
+              </Box>
+            </MenuItem>
+          </Box>
+        </Menu>
+
+        <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />
         <SearchIcon />
         <InputBase
           sx={{ ml: 1, flex: 1 }}
           placeholder="Search for a restaurant, cuisine, or dish"
-          inputProps={{ "aria-label": "search google maps" }}
         />
       </Search>
     </Box>
@@ -51,8 +103,12 @@ const Search = styled("div")(({ theme }) => ({
 
 const styles = {
   location: {
-      width: "11.25rem",
-      ml: 1,
+    width: "11.25rem",
+    ml: 1,
+  },
+  menuTrigger: {
+    p: 0,
+    "&:hover": { bgcolor: "white" },
   },
 };
 
