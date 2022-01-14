04d190eb351e8a386f60d6aa58a1b19f32eb5ec3
jason <2B3>
2022-01-14 08:28:53 -0500

    cuisine cards styled for mobile

--- src/page-blocks/home/Cuisines.js
+++ src/page-blocks/home/Cuisines.js
@@ -3,6 +3,7 @@ import { mix } from "../../../styles/styleMixins";
 //  prettier-ignore
 import { Typography, ButtonBase, Box } from '@mui/material';
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import LayoutContainer from "../../custom-components/LayoutContainer";
 //  prettier-ignore
 import { Ca, Cn, Fr, Gr, In, It, Jp, Mx, Pe, Es, Lk, Sy, Th, Us, Vn} from "react-flags-select";
 console.log(Ca);
@@ -26,33 +27,51 @@ const cuisineList = {
 export default function Cuisines() {
   return (
     <>
-      <Typography variant="h2">Popular cuisines around your area</Typography>
+      <LayoutContainer>
+        <Typography variant="h2">Popular cuisines around your area</Typography>
+      </LayoutContainer>
       {/* LIST OF CUISINE CARDS */}
-      {Object.keys(cuisineList).map((key, index) => {
-        // Use props to properly size the flag SVG's
-        const nationSVG = cuisineList[key]({ width: "3rem", height: "2rem" });
-        return (
-          <ButtonBase
-            key={index}
-            sx={{
-              display: "grid",
-              gridTemplateColumns: "auto 1fr auto",
-              border: "1px solid rgb(232,232,232)",
-              padding: "1.25rem 1.5rem",
-              width: "18rem",
-              "&:hover": {
-                boxShadow: `rgba(28, 28, 28, 0.08) 0px 4px 8px`,
-              },
-            }}
-          >
-            {nationSVG}
-            <Typography variant="h5" align="left" component="p" sx={{ ml: 2, mt: 0.6 }}>
-              {key}
-            </Typography>
-            <ChevronRightIcon sx={{ ml: 2, mt: 0.6 }} />
-          </ButtonBase>
-        );
-      })}
+      <Box
+        sx={{
+          ["@media (min-width: 500px)"]: {
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            gap: "1rem",
+            px: 2,
+          },
+        }}
+      >
+        {Object.keys(cuisineList).map((key, index) => {
+          // Use props to properly size the flag SVG's
+          const nationSVG = cuisineList[key]({ width: "3rem", height: "2rem" });
+          return (
+            <ButtonBase
+              key={index}
+              sx={{
+                display: "grid",
+                gridTemplateColumns: "auto 1fr auto",
+                width:"100%",
+                border: "1px solid rgb(232,232,232)",
+                padding: "1.25rem 1.5rem",
+                "&:hover": {
+                  boxShadow: `rgba(28, 28, 28, 0.08) 0px 4px 8px`,
+                },
+              }}
+            >
+              {nationSVG}
+              <Typography
+                variant="h5"
+                align="left"
+                component="p"
+                sx={{ ml: 2, mt: 0.6 }}
+              >
+                {key}
+              </Typography>
+              <ChevronRightIcon sx={{ ml: 2, mt: 0.6 }} />
+            </ButtonBase>
+          );
+        })}
+      </Box>
     </>
   );
 }
