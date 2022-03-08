2c498da810cb09be575de55ac87834e4e59dfa58
jason <2B3>
2022-03-08 08:01:13 -0500

    Added open hours

--- src/page-blocks/businessID/Hours.js
+++ src/page-blocks/businessID/Hours.js
@@ -3,60 +3,62 @@ import { Typography, Box, Stack } from "@mui/material";
 
 export default function Hours({ hours }) {
   return (
-    <Stack sx={styles.container}>
-      <Typography sx={{ gridArea: "a" }} variant="p">
-        Mon
-      </Typography>
-      <Typography sx={{ gridArea: "b" }} variant="p">
-        {hours.Monday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "d" }} variant="p">
-        Tues
-      </Typography>
-      <Typography sx={{ gridArea: "e" }} variant="p">
-        {hours.Tuesday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "g" }} variant="p">
-        Wed
-      </Typography>
-      <Typography sx={{ gridArea: "h" }} variant="p">
-        {hours.Wednesday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "j" }} variant="p">
-        Thurs
-      </Typography>
-      <Typography sx={{ gridArea: "k" }} variant="p">
-        {hours.Thursday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "m" }} variant="p">
-        Fri
-      </Typography>
-      <Typography sx={{ gridArea: "n" }} variant="p">
-        {hours.Friday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "p" }} variant="p">
-        Sat
-      </Typography>
-      <Typography sx={{ gridArea: "q" }} variant="p">
-        {hours.Saturday.join(" - ")}
-      </Typography>
-
-      <Typography sx={{ gridArea: "s" }} variant="p">
-        Sun
-      </Typography>
-      <Typography sx={{ gridArea: "t" }} variant="p">
-        {hours.Sunday.join(" - ")}
-      </Typography>
+    <Stack sx={styles.parent}>
+      <Typography sx={{mb:1, fontWeight: 600}} variant="h4">Hours</Typography>
+      <Box sx={styles.gridContainer}>
+        <Typography sx={{ gridArea: "a" }} variant="p">
+          Mon
+        </Typography>
+        <Typography sx={{ gridArea: "b" }} variant="p">
+          {hours.Monday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "d" }} variant="p">
+          Tues
+        </Typography>
+        <Typography sx={{ gridArea: "e" }} variant="p">
+          {hours.Tuesday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "g" }} variant="p">
+          Wed
+        </Typography>
+        <Typography sx={{ gridArea: "h" }} variant="p">
+          {hours.Wednesday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "j" }} variant="p">
+          Thurs
+        </Typography>
+        <Typography sx={{ gridArea: "k" }} variant="p">
+          {hours.Thursday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "m" }} variant="p">
+          Fri
+        </Typography>
+        <Typography sx={{ gridArea: "n" }} variant="p">
+          {hours.Friday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "p" }} variant="p">
+          Sat
+        </Typography>
+        <Typography sx={{ gridArea: "q" }} variant="p">
+          {hours.Saturday.join(" - ")}
+        </Typography>
+        <Typography sx={{ gridArea: "s" }} variant="p">
+          Sun
+        </Typography>
+        <Typography sx={{ gridArea: "t" }} variant="p">
+          {hours.Sunday.join(" - ")}
+        </Typography>
+      </Box>
     </Stack>
   );
 }
 const styles = {
-  container: {
+  parent: {
+    px: 2,
+    mt: 2,
+    fontSize: "1rem",
+  },
+  gridContainer: {
     display: "grid",
     gridTemplateColumns: "repeat(3, auto) 1fr",
     gridTemplateRows: "repeat(7, auto)",
@@ -69,11 +71,5 @@ const styles = {
     "p q r z5"
     "s t u z6"`,
     gridGap: "0.5rem 2rem" ,
-    px: 4,
-    mt: 4,
-    fontSize: "1rem",
-    ["@media (min-width: 500px)"]: {
-      fontSize: "1.25rem",
-    }
   },
 };
