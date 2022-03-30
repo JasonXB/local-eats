f34adf46c5e916705cd561b216219a76517ccab2
jason <2B3>
2022-03-30 02:27:49 -0400

    Edited next.config to remove a certain warning that appears on the homepage due to SSR and styled components
    https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman/71100071#:~:text=or%20you%20could%20just%20add

--- next.config.js
+++ next.config.js
@@ -3,10 +3,16 @@ module.exports = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+      use: ["@svgr/webpack"],
+    });
 
-    return config
+    return config;
   },
   reactStrictMode: true,
-}
+  // Fixes a warning on the homepage
+  // https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman/71100071#:~:text=or%20you%20could%20just%20add
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
