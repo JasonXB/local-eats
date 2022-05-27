00b2cfede7e99c2e014007e9c04f89b1699cbf03
JasonXB <jasonxportfolio@gmail.com>
2022-05-26 23:59:46 -0400

    Edited location denial modal and added an option telling users to change their browser settings

--- src/custom-components/Modals/LocationDenial/DenialModal.js
+++ src/custom-components/Modals/LocationDenial/DenialModal.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import { Typography, Divider } from "@mui/material";
 import { breakBefore } from "../../ConditionalBreak";
 import { useSelector } from "react-redux";
@@ -20,16 +20,22 @@ export default function LocationDenialModal(props) {
       </Typography>
       <Divider sx={{ my: 2 }} />
       <Typography variant="h5" component="p" sx={{ fontWeight: "600" }}>
-        ALT OPTION 1:
+        OPTION 1:
       </Typography>
       <Typography variant="h6" component="p">
         Search for restaurants in{breakBefore(490)} predetermined locations
       </Typography>
       <Typography variant="h5" component="p" sx={{ fontWeight: "600", mt: 2 }}>
-        ALT OPTION 2:
+        OPTION 2:
       </Typography>
       <Typography variant="h6" component="p">
         Specify any location in Canada or the US
+      </Typography>
+      <Typography variant="h5" component="p" sx={{ fontWeight: "600", mt: 2 }}>
+        OPTION 3:
+      </Typography>
+      <Typography variant="h6" component="p">
+        Edit browser settings and allow the site to access your location
       </Typography>
     </PredeterminedModalWrapper>
   );
